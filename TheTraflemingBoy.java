1db567c659c79afd8bc36470044134e65b048a68
ComputerKidMan <153094689+ComputerKidMan@users.noreply.github.com>
2024-05-22 12:07:00 -0700

    Update TheTraflemingBoy.java

--- TheTraflemingBoy.java
+++ TheTraflemingBoy.java
@@ -1,57 +1,25 @@
-public class TheTraflemingBoy extends GameCharacter{
-private int size;
-private int speed;
-int x =;
-int y  = 0;
-private int hp;
-	public TheTraflemingBoy(int x, int y, int size, int size, int speed, int hp){
-		this.x = x;
-		this.y = y;
-		this.size = size;
-		this.speed = speed;
-		this.hp = hp;
-		}
-	public void setSize(int size){
-		this.size = size;
-		}
-	public void setSpeed(int speed){
-		this.speed = speed;
-		}
-	public void setLocation(int[][] location){
-		this.location = location;
-		}
-	public void setColor(Color color){
-		this.color = color;
-		}
-	public void setHP(int hp){
-		this.hp = hp;
-		}
-	public drawMe(Graphics g2){
-		g2.drawImage(new ImageIcon("MainCharacter.png", x, y, size, size, null);
-	}
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class TheTraflemingBoy extends GameCharacter {
+    public static int size;
+    public static int speed;
+    public static int x;
+    public static int y;
+    public static int hp;
+
+    public static void setValues(int x, int y, int size, int speed, int hp) {
+
+        TheTraflemingBoy.x = x;
+        TheTraflemingBoy.y = y;
+        TheTraflemingBoy.size = size;
+        TheTraflemingBoy.speed = speed;
+        TheTraflemingBoy.hp = hp;
+    }
+
+    public static void drawMe(Graphics2D g2){
+        g2.drawImage(new ImageIcon("MainCharacterFront.png").getImage(), x, y, size, size, null);
+    }
 }
-
-class Key extends KeyAdapter
-{
-	boolean right = false;
-	boolean up = false;
-	boolean down = false;
-	boolean left = false;
-	MainCharacter MC = MainCharacter();
-	
-public void keyPressed(KeyEvent e)
-{
-if(e.getKeyCode() == KeyEvent.VK_RIGHT && !right)
-{
-}
-
-
-if(e.getKeyCode() == KeyEvent.VK_UP && !up)
-{
-}
-
-
-if(e.getKeyCode() == KeyEvent.VK_DOWN && !down)
-{
-}
-
