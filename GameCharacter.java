66bdd384a67967791104d4174db0941b5dc8c6c6
ComputerKidMan <153094689+ComputerKidMan@users.noreply.github.com>
2024-06-02 23:32:17 -0400

    Update GameCharacter.java

--- GameCharacter.java
+++ GameCharacter.java
@@ -2,8 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameCharacter {
-    public static Image[] getEntityImage = {new ImageIcon("MainCharacterFront.png").getImage(), new ImageIcon("Zombie.png").getImage(), new ImageIcon("Ghost.png").getImage()};
-    public static void drawCharacter(Graphics2D g2, int type, int x, int y, int size){
+    public static Image[] getEntityImage = {new ImageIcon("MainCharacterFront.png").getImage(), new ImageIcon("Zombie.png").getImage(), new ImageIcon("Ghost.png").getImage(), new ImageIcon("RedSquare.png").getImage()};
+    public void drawCharacter(Graphics2D g2, int type, int x, int y, int size){
         g2.drawImage(getEntityImage[type], x, y, size, size, null);
     }
 
